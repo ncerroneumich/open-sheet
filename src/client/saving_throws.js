13e470bb9f2aae94a48026f0eb96499c6efeef15
Nick Cerrone <nickolasdcerrone@gmail.com>
2023-07-05 16:38:43 -0400

    Add proficiency label to saving throws

--- src/client/saving_throws.js
+++ src/client/saving_throws.js
@@ -8,25 +8,33 @@ export default function SavingThrows(props) {
   if (Object.keys(props).length > 0) {
     return (
       <div className="saving-throw-box">
-        <p className="st-text">Saving Throws</p>
+        <div className="st-text">Saving Throws</div>
         <div className="st-row">
+          <div className={`st-proficiency-label-${Number(saving_throws.str.proficient)}`}></div>
           <div className="st-box" id="st-str">
             STR {display_modifier(saving_throws.str.value)}
           </div>
+          <div className={`st-proficiency-label-${Number(saving_throws.dex.proficient)}`}></div>
           <div className="st-box" id="st-dex">
             DEX {display_modifier(saving_throws.dex.value)}
           </div>
+          <div className={`st-proficiency-label-${Number(saving_throws.con.proficient)}`}></div>
           <div className="st-box" id="st-con">
             CON {display_modifier(saving_throws.con.value)}
           </div>
         </div>
         <div className="st-row">
+          <div className={`st-proficiency-label-${Number(saving_throws.int.proficient)}`}></div>
           <div className="st-box" id="st-int">
             INT {display_modifier(saving_throws.int.value)}
           </div>
+        <div className={`st-proficiency-label-${Number(saving_throws.wis.proficient)}`}></div>
+
           <div className="st-box" id="st-wis">
             WIS {display_modifier(saving_throws.wis.value)}
           </div>
+        <div className={`st-proficiency-label-${Number(saving_throws.cha.proficient)}`}></div>
+
           <div className="st-box" id="st-cha">
             CHA {display_modifier(saving_throws.cha.value)}
           </div>
