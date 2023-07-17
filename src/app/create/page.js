e59cff2e7ffb5cc7a42f29d4f77a5d4773968692
Nick Cerrone <nickolasdcerrone@gmail.com>
2023-07-17 16:44:53 -0400

    Add panel to character creation page

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,22 +1,79 @@
-// Creation steps
-// 0. Build Settings
-// Name, HP type
-// 1. 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+"use client";
 
+import '@/style/app.css'
+import { useState } from 'react';
+
+export default function Create() {
+    const [selected_tab, setSelectedTab] = useState(1);
+    const [name, setName] = useState("")
+
+    function handleTabClick(tab_num) {
+        setSelectedTab(tab_num);
+    }
+
+    function BasicsContent() {
+        return (
+            <input value={name} type='text'></input>
+        );
+    }
+
+    function RaceContent() {
+        return (
+            <p>Race Page</p>
+        );
+    }
+
+    function ClassContent() {
+        return (
+            <p>Class Page</p>
+        );
+    }
+
+    function BackgroundContent() {
+        return (
+            <p>Background Page</p>
+        );
+    }
+
+    function ASContent() {
+        return (
+            <p>Ability Scores Page</p>
+        );
+    }
+
+    function EquipmentContent() {
+        return (
+            <p>Equipment Page</p>
+        );
+    }
+
+    function BioContent() {
+        return (
+            <p>Bio Page</p>
+        );
+    }
+
+    return (
+        <div className="create-panel">
+            <nav className="panel-nav">
+                <button className='create-panel-button' href='#menu' onClick={() => handleTabClick(1)}>Basics</button>
+                <button className='create-panel-button' href='#race' onClick={() => handleTabClick(2)}>Race</button>
+                <button className='create-panel-button' href='#class' onClick={() => handleTabClick(3)}>Class</button>
+                <button className='create-panel-button' href='#background' onClick={() => handleTabClick(4)}>Background</button>
+                <button className='create-panel-button' href='#as' onClick={() => handleTabClick(5)}>Ability Scores</button>
+                <button className='create-panel-button' href='#equipment' onClick={() => handleTabClick(6)}>Equipment</button>
+                <button className='create-panel-button' href='#bio' onClick={() => handleTabClick(7)}>Bio</button>
+                <button>Done!</button>
+            </nav>
+            <div className="panel-content">
+                {selected_tab === 1 && <BasicsContent></BasicsContent>}
+                {selected_tab === 2 && <RaceContent></RaceContent>}
+                {selected_tab === 3 && <ClassContent></ClassContent>}
+                {selected_tab === 4 && <BackgroundContent></BackgroundContent>}
+                {selected_tab === 5 && <ASContent></ASContent>}
+                {selected_tab === 6 && <EquipmentContent></EquipmentContent>}
+                {selected_tab === 7 && <BioContent></BioContent>}
+            </div>
+        </div>
+    );
+}
